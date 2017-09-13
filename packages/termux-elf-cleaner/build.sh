4a3bb83021c32410ce99e7d06f61c62438871cc9
Vladimir Oltean <olteanv@gmail.com>
2017-09-13 12:44:16 +0300

    packages:termux-elf-cleaner: fork with a version that does not strip DT_RUNPATH
    * TODO: In the meantime, the termux-elf-cleaner version was bumped upstream to 1.2.
      Please revisit this commit and rebase termux-elf-cleaner.
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- packages/termux-elf-cleaner/build.sh
+++ packages/termux-elf-cleaner/build.sh
@@ -3,7 +3,9 @@ TERMUX_PKG_DESCRIPTION="Cleaner of ELF files for Android"
 # NOTE: The termux-elf-cleaner.cpp file is used by build-package.sh
 #       to create a native binary. Bumping this version will need
 #       updating the checksum used there.
-TERMUX_PKG_VERSION=1.2
-TERMUX_PKG_SRCURL=https://github.com/termux/termux-elf-cleaner/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=956d35778bf40523543a98ecec79d7d895a08ac69da1018acb84658a1c94e82b
+TERMUX_PKG_VERSION=1.1
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/LineageOSPlus/termux-elf-cleaner/archive/v${TERMUX_PKG_VERSION}-lineageplus.tar.gz
+TERMUX_PKG_SHA256=a30ac823f8a368682a162ce4eac3f99fd49535eeda42885b0fd3297192ef4b9e
+TERMUX_PKG_FOLDERNAME=termux-elf-cleaner-${TERMUX_PKG_VERSION}-lineageplus
 TERMUX_PKG_BUILD_IN_SRC=yes
