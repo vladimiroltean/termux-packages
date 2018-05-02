1380c2d4324425857842c6171bc27f9fa136b340
Vladimir Oltean <olteanv@gmail.com>
2018-05-02 23:41:31 +0300

    build-package: rename @TERMUX_HOME@ to simply @HOME@, in patches
    * The general convention so far was that TERMUX_-prefixed stuff was reserved
      for build-system-related variables. @HOME@ is a target-related variable.
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- build-package.sh
+++ build-package.sh
@@ -784,7 +784,7 @@ termux_step_patch_package() {
 	for patch in $TERMUX_PKG_BUILDER_DIR/*.patch{$TERMUX_ARCH_BITS,}; do
 		test -f "$patch" && \
 			sed "s%\@TERMUX_DESTDIR\@%${TERMUX_DESTDIR}%g" "$patch" | \
-			sed "s%\@TERMUX_HOME\@%${TERMUX_ANDROID_HOME}%g" | \
+			sed "s%\@HOME\@%${TERMUX_ANDROID_HOME}%g" | \
 			sed "s%\@USR\@%${USR}%g" | \
 			sed "s%\@VAR\@%${VAR}%g" | \
 			sed "s%\@ETC\@%${ETC}%g" | \
