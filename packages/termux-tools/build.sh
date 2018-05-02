1c48a58a72f9e7f70db3a47425aa3a0fada25b7a
Vladimir Oltean <olteanv@gmail.com>
2018-05-02 23:45:35 +0300

    packages:termux-tools: make it behave
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- packages/termux-tools/build.sh
+++ packages/termux-tools/build.sh
@@ -4,14 +4,14 @@ TERMUX_PKG_VERSION=0.54
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 TERMUX_PKG_ESSENTIAL=yes
 TERMUX_PKG_DEPENDS="termux-am"
-TERMUX_PKG_CONFFILES="etc/motd"
+TERMUX_PKG_CONFFILES="${ETC}/motd"
 
 termux_step_make_install () {
-	mkdir -p $TERMUX_PREFIX/bin/applets
+	mkdir -p ${TERMUX_DESTDIR}/${USR}/bin/applets
 	# Remove LD_LIBRARY_PATH from environment to avoid conflicting
 	# with system libraries that system binaries may link against:
 	for tool in df getprop logcat ping ping6 ip pm settings; do
-		WRAPPER_FILE=$TERMUX_PREFIX/bin/$tool
+		WRAPPER_FILE=${TERMUX_DESTDIR}/${USR}/bin/$tool
 		echo '#!/bin/sh' > $WRAPPER_FILE
 		echo 'unset LD_LIBRARY_PATH LD_PRELOAD' >> $WRAPPER_FILE
 		# Some tools require having /system/bin/app_process in the PATH,
@@ -21,10 +21,9 @@ termux_step_make_install () {
 		chmod +x $WRAPPER_FILE
 	done
 
-	cp -p $TERMUX_PKG_BUILDER_DIR/{dalvikvm,su,termux-fix-shebang,termux-reload-settings,termux-setup-storage,chsh,termux-open-url,termux-wake-lock,termux-wake-unlock,login,pkg,termux-open,termux-info} $TERMUX_PREFIX/bin/
-	perl -p -i -e "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" $TERMUX_PREFIX/bin/dalvikvm
+	cp -p $TERMUX_PKG_BUILDER_DIR/{dalvikvm,su,termux-fix-shebang,termux-reload-settings,termux-setup-storage,chsh,termux-open-url,termux-wake-lock,termux-wake-unlock,login,pkg,termux-open,termux-info} ${TERMUX_DESTDIR}/${USR}/bin/
+	termux_fixup_target_paths ${TERMUX_DESTDIR}/${USR}/bin/dalvikvm
 
-	cp $TERMUX_PKG_BUILDER_DIR/motd $TERMUX_PREFIX/etc/motd
-	cd $TERMUX_PREFIX/bin
-	ln -s -f termux-open xdg-open
+	cp $TERMUX_PKG_BUILDER_DIR/motd ${TERMUX_DESTDIR}/${ETC}/motd
+	(cd ${TERMUX_DESTDIR}/${USR}/bin; ln -s -f termux-open xdg-open)
 }
