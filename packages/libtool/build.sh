05d69c61c8d2819cc5feb64c90fe735faa556649
Vladimir Oltean <olteanv@gmail.com>
2018-05-02 23:44:19 +0300

    packages:libtool: make it behave
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- packages/libtool/build.sh
+++ packages/libtool/build.sh
@@ -8,7 +8,3 @@ TERMUX_PKG_DEPENDS="bash,grep,sed,libltdl"
 TERMUX_PKG_NO_DEVELSPLIT=yes
 TERMUX_PKG_CONFLICTS="libtool-dev"
 TERMUX_PKG_REPLACES="libtool-dev"
-
-termux_step_post_make_install () {
-	perl -p -i -e "s|\"/bin/|\"$TERMUX_PREFIX/bin/|" $TERMUX_PREFIX/bin/{libtool,libtoolize}
-}
